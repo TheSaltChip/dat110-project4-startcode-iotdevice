def2c8dedc915606b0d831d66ee627b2c053ee0e
TheSaltChip <seb2500@gmail.com>
2021-05-04 19:52:53 +0200

    Done i think, things work with postman and with the programs

--- ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/Configuration.java
+++ ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/Configuration.java
@@ -3,7 +3,7 @@ package no.hvl.dat110.aciotdevice.client;
 public class Configuration {
 
 	// specification of the host and port where the cloud service is running
-	public static int port = 8080;
+	public static int port = 8090;
     public static String host = "localhost";
 
 }
--- ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
+++ ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
@@ -1,27 +1,53 @@
 package no.hvl.dat110.aciotdevice.client;
 
+import com.google.gson.Gson;
+import okhttp3.*;
+
+import java.io.IOException;
+
 public class RestClient {
 
-	public RestClient() {
-		// TODO Auto-generated constructor stub
-	}
+    private final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    private final String URL = "http://" + Configuration.host + ":" + Configuration.port;
 
-	private static String logpath = "/accessdevice/log";
+    public RestClient() {
+        // TODO Auto-generated constructor stub
+    }
 
-	public void doPostAccessEntry(String message) {
+    private static String logpath = "/accessdevice/log/";
 
-		// TODO: implement a HTTP POST on the service to post the message
-		
-	}
-	
-	private static String codepath = "/accessdevice/code";
-	
-	public AccessCode doGetAccessCode() {
+    public void doPostAccessEntry(String message) {
+        // TODO: implement a HTTP POST on the service to post the message
+        RequestBody body = RequestBody.create(JSON, new Gson().toJson(new AccessMessage(message)));
 
-		AccessCode code = null;
-		
-		// TODO: implement a HTTP GET on the service to get current access code
-		
-		return code;
-	}
+        Request request = new Request.Builder().url(URL + logpath).post(body).build();
+
+        try (Response response = new OkHttpClient().newCall(request).execute()) {
+            System.out.println(response.body().string());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String codepath = "/accessdevice/code";
+
+    public AccessCode doGetAccessCode() {
+
+        AccessCode code = null;
+
+        // TODO: implement a HTTP GET on the service to get current access code
+
+        Request request = new Request.Builder()
+                .url(URL + codepath)
+                .get()
+                .build();
+
+        try (Response response = new OkHttpClient().newCall(request).execute()) {
+            code = new Gson().fromJson(response.body().string(), AccessCode.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return code;
+    }
 }
